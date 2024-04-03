326fc81c1ab5e72654a956d3e814f633a4ce18b9
Cannizzaro_reaction <157903250+epi-z-phany@users.noreply.github.com>
2024-04-04 01:39:45 +0800

    Create fasta_test.py

---
+++ tests/fasta_test.py
@@ -0,0 +1,24 @@
+import pytest
+from fasta import Fasta
+
+my_instance = Fasta('sequence.gb')        # 创建Fasta()实例
+
+def test_organism():
+    result = my_instance.find_organism()
+    assert result == 'Saccharomyces cerevisiae S288C'
+
+def test_chromosome():
+    result = my_instance.find_chromosome()
+    assert result == 'chromosome I'
+
+def test_dna_info():
+    result_list = my_instance.dna_info()
+    sliced_list = result_list[:3] + result_list[-3:]        # 由于原结果过长，难以编写test期望答案，故进行头尾切割
+    test_list = ['complement(1807..2169)\n', 'gene PAU8\n', 'locus YAL068C\n', '225460..226863\n', 'gene PHO11\n', 'locus YAR071W\n']
+    assert sliced_list == test_list
+
+def test_find_seq():
+    result_list = my_instance.find_dna_seq()
+    sliced_list = result_list[:1] + result_list[-1:]        # 由于原结果过长，难以编写test期望答案，故进行头尾切割
+    test_list = ['        1 ccacaccaca cccacacacc cacacaccac accacacacc acaccacacc cacacacaca', '   230161 ggtgtgggtg tgggtgtggt gtggtgtgtg ggtgtggtgt gggtgtggtg tgtgtggg']
+    assert sliced_list == test_list
