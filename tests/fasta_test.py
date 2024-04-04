b1cc8e76bc86e2719102db67ad23dd22d8045863
Cannizzaro_reaction <157903250+epi-z-phany@users.noreply.github.com>
2024-04-04 11:02:08 +0800

    Update fasta_test.py

--- tests/fasta_test.py
+++ tests/fasta_test.py
@@ -13,18 +13,25 @@ def test_chromosome():
 
 def test_dna_info():
     result_list = my_instance.dna_info()
-    sliced_list = result_list[:3] + result_list[-3:]        # 由于原结果过长，难以编写test期望答案，故进行头尾切割
+    sliced_list = result_list[:3] + result_list[-3:]        # 由于原结果过长，难以编写test期望答案，故进行头尾切割（下同）
     test_list = ['complement(1807..2169)\n', 'gene PAU8\n', 'locus YAL068C\n', '225460..226863\n', 'gene PHO11\n', 'locus YAR071W\n']
     assert sliced_list == test_list
 
 def test_find_seq():
     result_list = my_instance.find_dna_seq()
-    sliced_list = result_list[:1] + result_list[-1:]        # 由于原结果过长，难以编写test期望答案，故进行头尾切割
+    sliced_list = result_list[:1] + result_list[-1:]
     test_list = ['        1 ccacaccaca cccacacacc cacacaccac accacacacc acaccacacc cacacacaca', '   230161 ggtgtgggtg tgggtgtggt gtggtgtgtg ggtgtggtgt gggtgtggtg tgtgtggg']
     assert sliced_list == test_list
 
-def dna_list():
+def test_dna_list():
     result_list = my_instance.nucleic_acid_list('dna_filter.txt')
     sliced_list = result_list[:10] + result_list[-10:]
     test_list = ['c', 'c', 'a', 'c', 'a', 'c', 'c', 'a', 'c', 'a', 't', 'g', 't', 'g', 't', 'g', 't', 'g', 'g', 'g']
+    assert sliced_list == test_list
+
+def test_fasta_file():
+    nucleic_acid = my_instance.nucleic_acid_list('dna_filter.txt')
+    result_list = my_instance.fasta_file('reduced_gene_info.txt', 'fasta.txt', nucleic_acid)
+    sliced_list = result_list[-2:]
+    test_list = ['atgttgaagtcagccgtttattcaattttagccgcttctttggttaatgcaggtaccatacccctcggaaagttatctgacattgacaaaatcggaactcaaacggaaattttcccatttttgggtggttctgggccatactactctttccctggtgattatggtatttctcgtgatttgccggaaagttgtgaaatgaagcaagtgcaaatggttggtagacacggtgaaagataccccactgtcagcaaagccaaaagtatcatgacaacatggtacaaattgagtaactataccggtcaattcagcggagcattgtctttcttgaacgatgactacgaatttttcattcgtgacaccaaaaacctagaaatggaaaccacacttgccaattcggtcaatgttttgaacccatataccggtgagatgaatgctaagagacacgctcgtgatttcttggcgcaatatggctacatggtcgaaaaccaaaccagttttgccgtttttacgtctaactcgaacagatgtcatgatactgcccagtatttcattgacggtttgggtgataaattcaacatatccttgcaaaccatcagtgaagccgagtctgctggtgccaatactctgagtgcccaccattcgtgtcctgcttgggacgatgatgtcaacgatgacattttgaaaaaatatgataccaaatatttgagtggtattgccaagagattaaacaaggaaaacaagggtttgaatctgacttcaagtgatgcaaacactttttttgcatggtgtgcatatgaaataaacgctagaggttacagtgacatctgtaacatcttcaccaaagatgaattggtccgtttctcctacggccaagacttggaaacttattatcaaacgggaccaggctatgacgtcgtcagatccgtcggtgccaacttgttcaacgcttcagtgaaactactaaaggaaagtgaggtccaggaccaaaaggtttggttgagtttcacccacgataccgatattctgaactatttgaccactatcggcataatcgatgacaaaaataacttgaccgccgaacatgttccattcatggaaaacactttccacagatcctggtacgttccacaaggtgctcgtgtttacactgaaaagttccagtgttccaatgacacctatgttagatacgtcatcaacgatgctgtcgttccaattgaaacctgttctactggtccagggttctcctgtgaaataaatgacttctacgactatgctgaaaagagagtagccggtactgacttcctaaaggtctgtaacgtcagcagcgtcagtaactctactgaattgacctttttctgggactggaataccaagcactacaacgacactttattaaaacagtaa', '>YAR071W:225460-226863	[organism=Saccharomyces cerevisiae S288C]	[chromosome=chromosome I]']
     assert sliced_list == test_list
